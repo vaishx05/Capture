74fc80da7509dfe26696464eadc3a076aada98b4
Vaishnavi Gupta <97888193+vaishx05@users.noreply.github.com>
2022-07-16 23:05:55 +0530

    Add files via upload

---
+++ server/models/postMessage.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const postSchema = mongoose.Schema({
+  title: String,
+  message: String,
+  creator: String,
+  name: String,
+  tags: [String],
+  selectedFile: String,
+  likes: {
+    type: [String],
+    default: [],
+  },
+  comments: {
+    type: [String],
+    default: [],
+  },
+  createdAt: {
+    type: Date,
+    default: new Date(),
+  },
+});
+
+// Model (Collection)
+const PostMessage = mongoose.model("PostMessage", postSchema);
+
+export default PostMessage;
