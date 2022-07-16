22e3ebeb68299e47883686a6e61c05e8757207ed
Vaishnavi Gupta <97888193+vaishx05@users.noreply.github.com>
2022-07-16 23:02:50 +0530

    Delete src directory

--- src/components/index.js
+++
@@ -1,5 +0,0 @@
-export { default as Auth } from "./Auth/Auth";
-export { default as Form } from "./Form/Form";
-export { default as Home } from "./Home/Home";
-export { default as Navbar } from "./Navbar/Navbar";
-export { default as Posts } from "./Posts/Posts";
